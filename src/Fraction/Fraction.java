3340bdd204708dd0199cd3a1382f516dbcdf9057
Ross Capdeville <ross@teamengine.org>
2017-01-22 09:03:31 -0800

    Auto commit

--- src/Fraction/Fraction.java
+++ src/Fraction/Fraction.java
@@ -11,97 +11,97 @@
 
 public interface Fraction {
 
-  // Query Operations
+   // Query Operations
 
-  /** 
-   * Sets the Numerator 
-   *
-   * @param d a double representation of the numerator
-   */
-  void setNum(double d);
+   /** 
+    * Sets the Numerator 
+    *
+    * @param d a double representation of the numerator
+    */
+   void setNum(double d);
 
-  /** 
-   * Sets the Denominator
-   *
-   * @param d a double representation of the denominator
-   */
-  void setDem(double d);
+   /** 
+    * Sets the Denominator
+    *
+    * @param d a double representation of the denominator
+    */
+   void setDem(double d);
 
-  /**
-   * Returns the Numerator
-   *
-   * @return the numerator of the Fraction
-   */
-  double getNum();
+   /**
+    * Returns the Numerator
+    *
+    * @return the numerator of the Fraction
+    */
+   double getNum();
 
-  /**
-   * Returns the Denominator
-   *
-   * @return the denominator of the Fraction
-   */
-  double getDem();
+   /**
+    * Returns the Denominator
+    *
+    * @return the denominator of the Fraction
+    */
+   double getDem();
 
-  // Calculation Operations 
+   // Calculation Operations 
 
-  /**
-   * Adds the argument to itself
-   *
-   * @param f a Fraction to be used in the calculation
-   * @return the sum of itself and the argument
-   */
-  Fraction add(Fraction f);
+   /**
+    * Adds the argument to itself
+    *
+    * @param f a Fraction to be used in the calculation
+    * @return the sum of itself and the argument
+    */
+   Fraction add(Fraction f);
 
-  /**
-   * Subtracts the argument from itself
-   *
-   * @param f a Fraction to be used in the calculation
-   * @return the difference of itself and the argument
-   */
-  Fraction subtract(Fraction f);
+   /**
+    * Subtracts the argument from itself
+    *
+    * @param f a Fraction to be used in the calculation
+    * @return the difference of itself and the argument
+    */
+   Fraction subtract(Fraction f);
 
-  /**
-   * Multiplies the argument to itself
-   *
-   * @param f a Fraction to be used in the calculation
-   * @return the product of itself and the argument
-   */
-  Fraction multiply(Fraction f);
+   /**
+    * Multiplies the argument to itself
+    *
+    * @param f a Fraction to be used in the calculation
+    * @return the product of itself and the argument
+    */
+   Fraction multiply(Fraction f);
 
-  /**
-   * Divides the itself by the argument 
-   *
-   * @param f a Fraction to be used in the calculation
-   * @return the quotient of itself and the argument
-   */
-  Fraction divide(Fraction f);
+   /**
+    * Divides the itself by the argument 
+    *
+    * @param f a Fraction to be used in the calculation
+    * @return the quotient of itself and the argument
+    */
+   Fraction divide(Fraction f);
 
-  // Utility Operations
+   // Utility Operations
 
-  /**
-   * Provides the reciprocal of itself
-   *
-   * @return the reciprocal of itself 
-   */
-  Fraction reciprocal();
+   /**
+    * Provides the reciprocal of itself
+    *
+    * @return the reciprocal of itself 
+    */
+   Fraction reciprocal();
 
-  /**
-   * Determines if two fractions are equal by comparing their
-   * simplified / reduced forms. 
-   *
-   * @param f a Fraction to be used in the calculation
-   * @return the reciprocal of itself 
-   */
-  boolean equals(Fraction f);
+   /**
+    * Determines if two fractions are equal by comparing their
+    * simplified / reduced forms. 
+    *
+    * @param f a Fraction to be used in the calculation
+    * @return the reciprocal of itself 
+    */
+   boolean equals(Fraction f);
 
-  /**
-   * Returns a string representation of the fraction
-   *
-   * @return a string representation of the fraction
-   */
-  String toString();
+   /**
+    * Returns a string representation of the fraction
+    *
+    * @return a string representation of the fraction
+    */
+   String toString();
 
-  /**
-   * Reduces the underlying representation of itself to the Least Common Denominator
-   */
-  void simplify();
+   /**
+    * Reduces the underlying representation of itself to the Least Common Denominator
+    */
+   void simplify();
 }
--- src/Fraction/MyFraction.java
+++ src/Fraction/MyFraction.java
@@ -1,10 +1,10 @@
 /*
-  Ross Capdeville
-  Jan 21, 2017
-
-  Implements the Fraction Interface
-
-*/
+ * Ross Capdeville
+ * Jan 21, 2017
+ *
+ * Implements the Fraction Interface
+ *
+ */
 
 import java.util.logging.*;
 
@@ -12,115 +12,115 @@ import java.util.logging.*;
 
 public class MyFraction implements Fraction {
 
-  // Use this to get some logging as necessary in the unit test output
-  private static final Logger lmsg = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+   // Use this to get some logging as necessary in the unit test output
+   private static final Logger lmsg = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-  // Prof. said to use double instead of int during class
-  private double num;
-  private double dem;
+   // Prof. said to use double instead of int during class
+   private double num;
+   private double dem;
 
-  // Instantiate a new fraction with a numerator and denominator 
-  public MyFraction(double num, double dem) {
-    this.num=num;
-    this.dem=dem;
-  }
+   // Instantiate a new fraction with a numerator and denominator 
+   public MyFraction(double num, double dem) {
+      this.num=num;
+      this.dem=dem;
+   }
 
-  public void setNum(double num) {
-    this.num=num;
-  }
+   public void setNum(double num) {
+      this.num=num;
+   }
 
-  public void setDem(double dem) {
-    this.dem=dem;
-  }
+   public void setDem(double dem) {
+      this.dem=dem;
+   }
 
-  public double getNum() {
-    return num;
-  }
+   public double getNum() {
+      return num;
+   }
 
-  public double getDem() {
-    return dem; 
-  }    
+   public double getDem() {
+      return dem; 
+   }    
 
-  public String toString() {
-    return(num + "/" + dem);
-  }
+   public String toString() {
+      return(num + "/" + dem);
+   }
 
-  public Fraction add(Fraction f) {
-    MyFraction result=new MyFraction(0,0);
-    int mylcm=lcm((int)this.getDem(), (int)f.getDem());
-    result.setDem(mylcm);
-    result.setNum(this.getNum()*mylcm/this.getDem() + f.getNum()*mylcm/f.getDem());
-    result.simplify();
-    return result;
-  }
+   public Fraction add(Fraction f) {
+      MyFraction result=new MyFraction(0,0);
+      int mylcm=lcm((int)this.getDem(), (int)f.getDem());
+      result.setDem(mylcm);
+      result.setNum(this.getNum()*mylcm/this.getDem() + f.getNum()*mylcm/f.getDem());
+      result.simplify();
+      return result;
+   }
 
-  public Fraction subtract(Fraction f) {
-    MyFraction result=new MyFraction(0,0);
-    int mylcm=lcm((int)this.getDem(), (int)f.getDem());
-    result.setDem(mylcm); 
-    result.setNum(this.getNum()*mylcm/this.getDem() - f.getNum()*mylcm/f.getDem());
-    result.simplify();
-    return result;
-  }
+   public Fraction subtract(Fraction f) {
+      MyFraction result=new MyFraction(0,0);
+      int mylcm=lcm((int)this.getDem(), (int)f.getDem());
+      result.setDem(mylcm); 
+      result.setNum(this.getNum()*mylcm/this.getDem() - f.getNum()*mylcm/f.getDem());
+      result.simplify();
+      return result;
+   }
 
-  public Fraction multiply(Fraction f) {
-    MyFraction result=new MyFraction(0,0);
-    result.setNum(this.getNum()*f.getNum());
-    result.setDem(this.getDem()*f.getDem());
-    result.simplify();
-    return result;
-  }
+   public Fraction multiply(Fraction f) {
+      MyFraction result=new MyFraction(0,0);
+      result.setNum(this.getNum()*f.getNum());
+      result.setDem(this.getDem()*f.getDem());
+      result.simplify();
+      return result;
+   }
 
-  public Fraction divide(Fraction f) {
-    MyFraction result=new MyFraction(0,0);
-    result.setNum(this.getNum()*f.getDem());
-    result.setDem(this.getDem()*f.getNum());
-    result.simplify();
-    return result;
-  }
+   public Fraction divide(Fraction f) {
+      MyFraction result=new MyFraction(0,0);
+      result.setNum(this.getNum()*f.getDem());
+      result.setDem(this.getDem()*f.getNum());
+      result.simplify();
+      return result;
+   }
 
-  public Fraction reciprocal() {
-    MyFraction result=new MyFraction(0,0);
-    double tmp = this.getNum();
-    result.setNum(this.getDem());
-    result.setDem(tmp);
-    return result;
-  }
+   public Fraction reciprocal() {
+      MyFraction result=new MyFraction(0,0);
+      double tmp = this.getNum();
+      result.setNum(this.getDem());
+      result.setDem(tmp);
+      return result;
+   }
 
-	/** 
-	 * Simplify a copy of myself and the argument, and compare them for
-	 * equality
-	 */
-  public boolean equals(Fraction f) {
-		MyFraction f1=new MyFraction(this.getNum(),this.getDem());
-		MyFraction f2=new MyFraction(f.getNum(), f.getDem());
-		f1.simplify();
-		f2.simplify();
-    return f1.getNum()==f2.getNum() && f1.getDem() == f2.getDem() ? true : false;
-  }
+   /** 
+    * Simplify a copy of myself and the argument, and compare them for
+    * equality
+    */
+   public boolean equals(Fraction f) {
+      MyFraction f1=new MyFraction(this.getNum(),this.getDem());
+      MyFraction f2=new MyFraction(f.getNum(), f.getDem());
+      f1.simplify();
+      f2.simplify();
+      return f1.getNum()==f2.getNum() && f1.getDem() == f2.getDem() ? true : false;
+   }
 
-  public void simplify() {
-    int n = (int)num;
-    int d = (int)dem;
-    if (n<0 && d<0) {
-      n*=-1;
-      d*=-1;
-    }
-    int mygcd=gcd(n,d);
-    num=(double)n/mygcd;
-    dem=(double)d/mygcd;
-  }
+   public void simplify() {
+      int n = (int)num;
+      int d = (int)dem;
+      if (n<0 && d<0) {
+         n*=-1;
+         d*=-1;
+      }
+      int mygcd=gcd(n,d);
+      num=(double)n/mygcd;
+      dem=(double)d/mygcd;
+   }
 
-  private static int lcm(int a, int b) {
-    return Math.abs(a*b)/gcd(a,b);
-  }
+   private static int lcm(int a, int b) {
+      return Math.abs(a*b)/gcd(a,b);
+   }
 
-  private static int gcd(int a, int b) {
-    while (b != 0) {
-      int mod = a % b;
-      a = b;
-      b = mod;
-    }
-    return a;
-  }
+   private static int gcd(int a, int b) {
+      while (b != 0) {
+         int mod = a % b;
+         a = b;
+         b = mod;
+      }
+      return a;
+   }
 }
