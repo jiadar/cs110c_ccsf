2b2bd2528d8d5d52d0135dde3fe9751bf75d7e12
Ross Capdeville <ross@teamengine.org>
2017-01-22 12:02:49 -0800

    Auto commit

--- src/resizableArrayBag/TestResizableArrayBag.java
+++ src/resizableArrayBag/TestResizableArrayBag.java
@@ -1,5 +1,13 @@
 package resizableArrayBag;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestResizableArrayBag {
 
+   @Test
+   public void testStub()
+   {
+      assertEquals(1,1);
+   }
 }
