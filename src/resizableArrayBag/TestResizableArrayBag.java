fc0afb51319f37017aa561b30f795b5a4b0f41ab
Ross Capdeville <ross@teamengine.org>
2017-01-27 11:37:50 -0800

    Auto commit

--- src/resizableArrayBag/TestResizableArrayBag.java
+++ src/resizableArrayBag/TestResizableArrayBag.java
@@ -2,21 +2,44 @@ package resizableArrayBag;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.logging.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TestResizableArrayBag {
 
+   private static final Logger lmsg = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
    @Test
-   public void testCurrentSize()
+   public void testCurrentSizeWithAdd()
    {
-      ResizableArrayBag<Integer> mybag1 = new ResizableArrayBag<Integer>();
-//      ResizableArrayBag mybag2 = new ResizableArrayBag(25);
-      //   ResizableArrayBag mybag3 = new ResizableArrayBag(125);
-//      assertEquals(mybag1.getCurrentSize(), 25);
-      //assertEquals(mybag2.getCurrentSize(), 25);
-      //assertEquals(mybag3.getCurrentSize(), 125);
-      assertEquals(1,1);
+      int i;
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>(25);
+      assertEquals(mybag.getCurrentSize(), 0);
+      for(i = 1; i < ThreadLocalRandom.current().nextInt(10, 100); ++i)
+      {
+         mybag.add(i);
+         assertEquals(mybag.getCurrentSize(), i);
+      }
    }
 
+   @Test
+
+   public void testAddRemoveContains()
+   {
+      int i;
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      for(i = 1; i < 20; ++i)
+      {
+         mybag.add(i);
+         for(j = 1; j <= i; ++j)
+         {
+            assertEquals(mybag.contains(i), true);
+            assertEquals(mybag.contains(i+1), false);
+         }
+      }
+      mybag.remove(17);
+      assertEquals(mybag.contains(17), false);
+   }
    @Test
    public void testStub()
    {
