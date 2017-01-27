775146c4a9c8cc67d10a150c984a2e1eee26e6ce
Ross Capdeville <ross@teamengine.org>
2017-01-27 11:48:36 -0800

    Auto commit

--- src/resizableArrayBag/TestResizableArrayBag.java
+++ src/resizableArrayBag/TestResizableArrayBag.java
@@ -24,9 +24,9 @@ public class TestResizableArrayBag {
 
    @Test
 
-   public void testAddRemoveContains()
+   public void testVarious()
    {
-      int i;
+      int i, j;
       ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
       for(i = 1; i < 20; ++i)
       {
@@ -37,9 +37,88 @@ public class TestResizableArrayBag {
             assertEquals(mybag.contains(i+1), false);
          }
       }
+      assertEquals(mybag.getCurrentSize(), 19);
+      mybag.remove(17);
+      assertEquals(mybag.contains(17), false);
+      mybag.add(17);
+      mybag.add(17);
+      assertEquals(mybag.contains(17), true);
+      mybag.remove(17);
+      assertEquals(mybag.contains(17), true);
       mybag.remove(17);
       assertEquals(mybag.contains(17), false);
    }
+
+   @Test
+   public void testAdd()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      mybag.add(1);
+      assertEquals(mybag.contains(1), true);
+   }
+
+   @Test
+   public void testRemove()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      mybag.add(1);
+      assertEquals(mybag.contains(1), true);
+      mybag.remove(1);
+      assertEquals(mybag.contains(1), false);
+   }
+
+   @Test
+   public void testEmpty()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      assertEquals(mybag.isEmpty(), true);
+      mybag.add(1);
+      assertEquals(mybag.isEmpty(), false);
+   }
+
+   @Test
+   public void testFrequency()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      assertEquals(mybag.getFrequencyOf(7), 0);
+      mybag.add(7);
+      assertEquals(mybag.getFrequencyOf(7), 1);
+      mybag.add(7);
+      assertEquals(mybag.getFrequencyOf(7), 2);
+   }
+   
+   @Test
+   public void testClear()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      mybag.add(7);
+      mybag.add(17);
+      mybag.add(123);
+      mybag.clear();
+      assertEquals(mybag.getCurrentSize(), 0);
+   }
+
+   @Test
+   public void testIndex()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>();
+      mybag.add(7);
+      mybag.add(17);
+      mybag.add(123);
+      assertEquals(mybag.getIndexOf(123), 2);
+      assertEquals(mybag.getIndexOf(17), 1);
+   }
+
+   @Test
+   public void testFull()
+   {
+      ResizableArrayBag<Integer> mybag = new ResizableArrayBag<Integer>(1);
+      assertEquals(mybag.isArrayFull(), false);
+      mybag.add(1);
+      assertEquals(mybag.isArrayFull(), true);
+   }
+
+
    @Test
    public void testStub()
    {
