079f5c6e9e7633e88151458c124eb8b16642e390
Ross Capdeville <ross@teamengine.org>
2017-02-06 17:17:59 -0800

    Auto commit

--- src/linkedBag/TestLinkedBag.java
+++ src/linkedBag/TestLinkedBag.java
@@ -44,7 +44,7 @@ public class TestLinkedBag {
       mybag.add(2);
       mybag.add(3);
       mybag.removeLast();
-      assertEquals(mybag.toString(), "3 -> 2 -> z ");            
+      assertEquals(mybag.toString(), "3 -> 2 -> ");            
    }
       
    @Test
