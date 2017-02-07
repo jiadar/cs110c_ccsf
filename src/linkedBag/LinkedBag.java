31e7dc119ced72634b64ebfc23294c81c26193d1
Ross Capdeville <ross@teamengine.org>
2017-02-06 17:19:49 -0800

    Auto commit

--- src/linkedBag/LinkedBag.java
+++ src/linkedBag/LinkedBag.java
@@ -50,7 +50,7 @@ public class LinkedBag<T> implements BagInterface<T>
 
       T rval = cur.data;
       lmsg.info("Returning: " + rval);
-      cur = null;
+      cur.data = null;
       return rval;
    }
    
