4470313afdeb21bccafabc14e5c871afbca865e9
Ross Capdeville <ross@teamengine.org>
2017-02-20 11:51:03 -0800

    Auto commit

--- src/sort/Sorts.java
+++ src/sort/Sorts.java
@@ -10,11 +10,16 @@ public class Sorts {
       if (data == null)
          return -1;
 
-      int min = 0;
+      int min = idx;
       for(int i = idx; i < data.length; ++i)
-         if (data[i].compareTo(data[min]) < 0)
+      {  
+         if (data[i].compareTo(data[idx]) < 0) 
+         {         
             min = i;
-
+            System.out.print("idx = " + idx + " i= " + i +  " min = " + data[min] + " ");
+            System.out.println(Arrays.toString(data));
+         }
+      }
       return min;      
    }
 
@@ -25,14 +30,16 @@ public class Sorts {
       data[idx2] = temp;
    }
 
-   public static <T extends Comparable<? super T>> void
+   public static <T extends Comparable<? super T>> int
    selectionSortSublist(T[] data, int idx)
    {
       int min = findMin(data, idx);
-      System.out.print("idx = " + idx + "  " + "min = " + data[min] + " ");
-      swapElts(data, idx, min);
-      System.out.println(Arrays.toString(data));
-      
+      if (data[idx].compareTo(data[min]) > 0)
+      {
+         swapElts(data, idx, min);
+         return 1;
+      }
+      return 0;
    }
    
    public static <T extends Comparable<? super T>> int
@@ -53,9 +60,8 @@ public class Sorts {
       int sortedIndex=0;
       while (sortedIndex < data.length)
       {
-         selectionSortSublist(data, sortedIndex);
+         count += selectionSortSublist(data, sortedIndex);
          ++count;
-         ++sortedIndex;
       }
       return count;
    }
