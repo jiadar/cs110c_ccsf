523bdecfb4caa25370b6986ced24e3e94440961a
Ross Capdeville <ross@teamengine.org>
2017-03-05 19:17:46 -0800

    Auto commit

--- src/queue/TestQueue.java
+++ src/queue/TestQueue.java
@@ -11,8 +11,110 @@ public class TestQueue {
    private static int fails=0;
    private static final Logger lmsg = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
+   // Start of Array Tests
+   
    @Test
-   public void testNewInstance()
+   public void testArrayNewInstance()
+   {
+      QueueInterface<Integer> q = new ArrayQueue<Integer>();
+      assertEquals(q.isEmpty(), true);
+      assertEquals(q.toString(), "empty");
+   }
+
+   @Test
+   public void testArrayEnqueue()
+   {
+      QueueInterface<Integer> q = new ArrayQueue<Integer>();
+      q.enqueue(1);
+      q.enqueue(2);
+      q.enqueue(3);
+      assertEquals(q.toString().substring(0,8), "[1, 2, 3");
+   }
+
+   @Test
+   public void testArrayGetFront()
+   {
+      QueueInterface<Integer> q = new ArrayQueue<Integer>();
+      q.enqueue(1);
+      assertEquals((Integer) q.getFront(), (Integer) 1);
+      q.enqueue(2);
+      assertEquals((Integer) q.getFront(), (Integer) 1);
+   }
+
+   @Test
+   public void testArrayDequeue()
+   {
+      QueueInterface<Integer> q = new ArrayQueue<Integer>();
+      q.enqueue(1);
+      assertEquals(q.toString(), "1, next");
+      q.enqueue(2);
+      assertEquals(q.toString(), "1, 2, next");
+      q.enqueue(3);
+      assertEquals(q.toString(), "1, 2, 3, next");
+      q.enqueue(4);
+      assertEquals(q.toString(), "1, 2, 3, 4, next");
+      q.dequeue();
+      assertEquals(q.toString(), "2, 3, 4, next, free");
+      q.dequeue();
+      assertEquals(q.toString(), "3, 4, next, free, free");
+      q.dequeue();
+      assertEquals(q.toString(), "4, next, free, free, free");
+      q.dequeue();
+      assertEquals(q.toString(), "empty");
+   }
+
+   @Test
+   public void testArrayClear()
+   {
+      QueueInterface<Integer> q = new ArrayQueue<Integer>();
+      q.enqueue(1);
+      q.enqueue(2);
+      assertEquals(q.toString().substring(0,5), "[1, 2");
+      q.clear();
+      assertEquals(q.isEmpty(), true);      
+   }
+
+   @Test
+   public void testArraySplice()
+   {
+      QueueInterface<Integer> q1 = new ArrayQueue<Integer>();
+      QueueInterface<Integer> q2 = new ArrayQueue<Integer>();
+      q1.enqueue(1);
+      q1.enqueue(2);
+      q1.enqueue(3);
+      q2.enqueue(4);
+      q2.enqueue(5);
+      q2.enqueue(6);
+      q1.splice(q2);
+      assertEquals(q1.toString(),"1, 2, 3, 4, 5, 6, next");
+   }
+
+   @Test
+   public void testArraySplice2()
+   {
+      ArrayQueue<Integer> q1 = new ArrayQueue<Integer>();
+      ArrayQueue<Integer> q2 = new ArrayQueue<Integer>();
+      q2.enqueue(4);
+      q2.enqueue(5);
+      q2.enqueue(6);
+      q1.splice2(q2);
+      assertEquals(q1.toString(),"4, 5, 6, next");
+      q1.clear();
+      q2.clear();
+      q1.enqueue(1);
+      q1.enqueue(2);
+      q1.enqueue(3);
+      q2.enqueue(4);
+      q2.enqueue(5);
+      q2.enqueue(6);
+      q1.splice2(q2);
+//      assertEquals(q1.toString(),"1, 2, 3, 4, 5, 6, next");
+   }
+
+   // Start of Linked Tests
+   
+   @Test
+   public void testLinkedNewInstance()
    {
       QueueInterface<Integer> q = new LinkedQueue<Integer>();
       assertEquals(q.isEmpty(), true);
@@ -20,7 +122,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testEnqueue()
+   public void testLinkedEnqueue()
    {
       QueueInterface<Integer> q = new LinkedQueue<Integer>();
       q.enqueue(1);
@@ -30,7 +132,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testGetFront()
+   public void testLinkedGetFront()
    {
       QueueInterface<Integer> q = new LinkedQueue<Integer>();
       q.enqueue(1);
@@ -40,7 +142,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testDequeue()
+   public void testLinkedDequeue()
    {
       QueueInterface<Integer> q = new LinkedQueue<Integer>();
       q.enqueue(1);
@@ -62,7 +164,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testClear()
+   public void testLinkedClear()
    {
       QueueInterface<Integer> q = new LinkedQueue<Integer>();
       q.enqueue(1);
@@ -73,7 +175,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testSplice()
+   public void testLinkedSplice()
    {
       QueueInterface<Integer> q1 = new LinkedQueue<Integer>();
       QueueInterface<Integer> q2 = new LinkedQueue<Integer>();
@@ -88,7 +190,7 @@ public class TestQueue {
    }
 
    @Test
-   public void testSplice2()
+   public void testLinkedSplice2()
    {
       LinkedQueue<Integer> q1 = new LinkedQueue<Integer>();
       LinkedQueue<Integer> q2 = new LinkedQueue<Integer>();
