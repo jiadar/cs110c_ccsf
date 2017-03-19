96bb28c55da176f485516c685da48f54e3130521
Ross Capdeville <ross@teamengine.org>
2017-03-19 12:38:47 -0700

    Auto commit

--- src/lcl/LinkedChainList.java
+++ src/lcl/LinkedChainList.java
@@ -1,86 +1,109 @@
 package lcl;
 
 import java.util.Iterator;
+import java.lang.UnsupportedOperationException;
+import java.lang.NullPointerException;
+import java.lang.NoSuchElementException;
+import java.lang.IllegalStateException;
 
 /**
  * Created by schatz on 10/21/15.
  */
 public class LinkedChainList<T> extends LinkedChainBase<T> implements ListInterface<T> {
 
-    public LinkedChainList() {
-        super();
-    }
+   public LinkedChainList() {
+      super();
+   }
 
-    public T remove(int givenPosition) {
-        if (givenPosition < 1 || givenPosition > getLength()) {
-            throw new IndexOutOfBoundsException("index: " + givenPosition);
-        }
+   public T remove(int givenPosition) {
+      if (givenPosition < 1 || givenPosition > getLength()) {
+         throw new IndexOutOfBoundsException("index: " + givenPosition);
+      }
 
-        if (givenPosition == 1) {
-            return removeFirstNode();
-        } else {
-            Node nodeBefore = traverseToNodeAt(givenPosition);
-            return removeAfterNode(nodeBefore);
-        }
-    }
+      if (givenPosition == 1) {
+         return removeFirstNode();
+      } else {
+         Node nodeBefore = traverseToNodeAt(givenPosition);
+         return removeAfterNode(nodeBefore);
+      }
+   }
 
-    public void add(T newEntry) {
-       add(getLength(), newEntry);
-    }
+   public void add(T newEntry) {
+      add(getLength(), newEntry);
+   }
 
-    public void add(int newPosition, T newEntry) {
-       if (isEmpty())
-          addFirstNode(new Node(newEntry));
-       else
-          addAfterNode(traverseToNodeAt(newPosition), new Node(newEntry));                                        
-    }
+   public void add(int newPosition, T newEntry) {
+      if (isEmpty())
+         addFirstNode(new Node(newEntry));
+      else
+         addAfterNode(traverseToNodeAt(newPosition), new Node(newEntry));                                        
+   }
 
-    public T replace(int givenPosition, T newEntry) {
-       addAfterNode(traverseToNodeAt(givenPosition), new Node(newEntry));
+   public T replace(int givenPosition, T newEntry) {
+      addAfterNode(traverseToNodeAt(givenPosition), new Node(newEntry));
 
-       if (givenPosition == 1)
-          return removeFirstNode();
+      if (givenPosition == 1)
+         return removeFirstNode();
 
-       return removeAfterNode(traverseToNodeAt(givenPosition-1));
+      return removeAfterNode(traverseToNodeAt(givenPosition-1));
 
-    }
+   }
 
-    public T getEntry(int givenPosition) {
-       return traverseToNodeAt(givenPosition).getData();
-    }
+   public T getEntry(int givenPosition) {
+      return traverseToNodeAt(givenPosition).getData();
+   }
 
-    public boolean contains(T anEntry) {
-        return false;
-    }
+   public boolean contains(T anEntry) {
+      return false;
+   }
     
-    public Iterator<T> iterator() {
-       private Node nextNode;
-       private Node curNode;
+   public Iterator<T> iterator() {
+      return new IteratorForLinkedChainList();
+   }
 
-       private Iterator()
-       {
-          if (isEmpty())
-             return null;
+   private class IteratorForLinkedChainList implements Iterator<T>
+   {
+      private Node prevNode = null;
+      private Node curNode = null;
+      private int count = 0;
+      
+      private IteratorForLinkedChainList()
+      {
+         if (isEmpty())
+            throw new NullPointerException("List is empty");
           
-          curNode=getFirstNode();
-          nextNode=curNode.next;
-       }
+         curNode=getFirstNode();
+      }
        
-       public boolean hasNext()
-       {
-          return curNode != null;
-       }
+      public boolean hasNext()
+      {
+         return curNode != null;
+      }
 
-       public T next()
-       {
-          curNode = curNode.next;
-          nextNode = nextNode.next;
-          return curNode;
-       }
+      public T next()
+      {
+         if ( ! hasNext())
+            throw new NoSuchElementException();
 
-       public void remove()
-       {
-          return new UnsupportedOperationException();
-       }
-    }
+         T data = curNode.getData();
+         prevNode = curNode;
+         curNode = curNode.getNextNode();
+         count++;
+         return data;
+      }
+
+      public void remove()
+      {
+         if (prevNode == null)
+            throw new IllegalStateException();
+         if (count == 1)
+            removeFirstNode();
+         else
+         {
+            prevNode.setNextNode(curNode.getNextNode());
+         }
+         count--;
+      }
+   }
 }
+
