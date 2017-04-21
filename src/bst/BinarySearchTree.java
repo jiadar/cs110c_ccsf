8f3109e29c22a986f27b3273aaaa2cce2a3481d6
Ross Capdeville <ross@teamengine.org>
2017-04-21 09:46:33 -0700

    Auto commit

---
+++ src/bst/BinarySearchTree.java
@@ -0,0 +1,19 @@
+package bst;
+
+class BinarySearchTree<T>
+{
+   private T data;
+   private BinarySearchTree<T> left;
+   private BinarySearchTree<T> right;
+
+   public BinarySearchTree(T value)
+   {
+      this.value = value;
+   }
+
+   public BinarySearchTree add(T value)
+   {
+      
+   }
+}
+
---
+++ src/bst/Main.java
@@ -0,0 +1,8 @@
+package bst;
+
+public class Main {
+   public static void main(String[] args)
+   {
+      System.out.println("Hello BST");
+   }
+}
