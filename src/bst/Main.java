bdb135153bb092b88943579a6932ccdec17b8b50
Ross Capdeville <ross@teamengine.org>
2017-04-24 15:11:49 -0700

    Auto commit

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -4,5 +4,9 @@ public class Main {
    public static void main(String[] args)
    {
       System.out.println("Hello BST");
+      Btree<Integer> b = new Btree<Integer>;
+      b.add(10);
+      b.add(5);
+      b.add(15);
    }
 }
