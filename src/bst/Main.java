40af3b0798ff1620997b9c3a857291d342b0a5f2
Ross Capdeville <ross@teamengine.org>
2017-04-25 11:14:51 -0700

    Auto commit

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
    public static void main(String[] args) {
-      doSearch();
+      //      doSearch();
+      
    }
 
    private static void doSearch() {
