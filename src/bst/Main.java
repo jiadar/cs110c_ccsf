741f3701c68c1c025f81c79f40681844512a7b70
Ross Capdeville <ross@teamengine.org>
2017-04-25 10:32:41 -0700

    Auto commit

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -6,20 +6,35 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
    public static void main(String[] args)
    {
-      System.out.println("Hello BST");
+      int SIZE=10000;
+      long startTime, endTime, duration;
+      System.out.println("Hello BST!");
       Btree<Integer>  b = new Btree<Integer>();
       ArrayList<Integer> a = new ArrayList<Integer>();
-      for(int i=0; i<100; i++) {
-         Integer num = ThreadLocalRandom.current().nextInt(100, 999);
+
+      System.out.println("Generating...");
+      for(int i=0; i<SIZE; i++) {
+         Integer num = ThreadLocalRandom.current().nextInt(1, SIZE*16);
          a.add(num);
          b.add(num);
       }
-      long startTime = System.nanoTime();
-      methodToTime();
-      long endTime = System.nanoTime();
-      long duration = (endTime - startTime); 
-      System.out.println(b.toString());
-      System.out.print("Get entry test: ");
-      System.out.println(b.getEntry(Integer.valueOf(6)));
+      System.out.println(a.toString());
+
+      System.out.println("Searching BST...");
+      startTime = System.nanoTime();
+      for(int i=0; i<1000; i++) {
+         b.contains(i);
+      }
+      endTime = System.nanoTime();
+      System.out.println("Binary Tree Search: " + (endTime - startTime)/1000000 + " ms");
+      //      System.out.println(b.toString());
+
+      System.out.println("Searching Array List...");
+      startTime = System.nanoTime();
+      for(int i=0; i<1000; i++) {
+         a.contains(i);  
+      }      
+      endTime = System.nanoTime();
+      System.out.println("Array List Search: " + (endTime - startTime)/1000000 + " ms");
    }
 }
