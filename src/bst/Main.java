5c9ce0f5a5d191e032489c36ba87e684a68b7079
Ross Capdeville <ross@teamengine.org>
2017-04-25 10:17:30 -0700

    Auto commit

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -1,18 +1,23 @@
 package bst;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
    public static void main(String[] args)
    {
       System.out.println("Hello BST");
       Btree<Integer>  b = new Btree<Integer>();
-      b.add(10);
-      b.add(5);
-      b.add(1);
-      b.add(7);
-      b.add(9);
-      b.add(6);
-      b.add(4);
-      b.add(3);
+      ArrayList<Integer> a = new ArrayList<Integer>();
+      for(int i=0; i<100; i++) {
+         Integer num = ThreadLocalRandom.current().nextInt(100, 999);
+         a.add(num);
+         b.add(num);
+      }
+      long startTime = System.nanoTime();
+      methodToTime();
+      long endTime = System.nanoTime();
+      long duration = (endTime - startTime); 
       System.out.println(b.toString());
       System.out.print("Get entry test: ");
       System.out.println(b.getEntry(Integer.valueOf(6)));
